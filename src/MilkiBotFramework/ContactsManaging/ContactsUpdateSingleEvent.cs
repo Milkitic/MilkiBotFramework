70034bfbd442b492ed2f2b50b4e8e5aa07d22498
Milkitic <Milkitic@126.com>
2022-03-07 14:06:23 +0800

    Update ContactsUpdateSingleEvent.cs

--- src/MilkiBotFramework/ContactsManaging/ContactsUpdateSingleEvent.cs
+++ src/MilkiBotFramework/ContactsManaging/ContactsUpdateSingleEvent.cs
@@ -1,8 +1,11 @@
-﻿using MilkiBotFramework.ContactsManaging.Models;
+﻿using System.Diagnostics;
+using System.Text;
+using MilkiBotFramework.ContactsManaging.Models;
 using MilkiBotFramework.ContactsManaging.Results;
 
 namespace MilkiBotFramework.ContactsManaging;
 
+[DebuggerDisplay("{DebuggerDisplay}")]
 public sealed class ContactsUpdateSingleEvent
 {
     public string? ChangedPath { get; init; }
@@ -12,4 +15,31 @@ public sealed class ContactsUpdateSingleEvent
     public ChannelInfo? SubChannelInfo { get; init; }
     public ContactsUpdateRole UpdateRole { get; init; }
     public ContactsUpdateType UpdateType { get; init; }
+
+    private string DebuggerDisplay
+    {
+        get
+        {
+            var sb = new StringBuilder($"Role={UpdateRole};Type={UpdateType};");
+            switch (UpdateRole)
+            {
+                case ContactsUpdateRole.Channel:
+                    sb.Append($"Id={ChannelInfo!.ChannelId}");
+                    break;
+                case ContactsUpdateRole.SubChannel:
+                    sb.Append($"Id={ChannelInfo!.ChannelId}.{ChannelInfo.SubChannelId}");
+                    break;
+                case ContactsUpdateRole.Member:
+                    sb.Append($"Id={MemberInfo!.ChannelId}.{MemberInfo.UserId}");
+                    break;
+                case ContactsUpdateRole.Private:
+                    sb.Append($"Id={PrivateInfo!.UserId}");
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+
+            return sb.ToString();
+        }
+    }
 }
