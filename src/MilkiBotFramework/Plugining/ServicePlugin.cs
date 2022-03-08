e223cfab51c4bd05addf4bc35518dd9997993fc6
Milkitic <Milkitic@126.com>
2022-03-08 17:11:51 +0800

    add exception parameter on OnPluginException()

--- src/MilkiBotFramework/Plugining/ServicePlugin.cs
+++ src/MilkiBotFramework/Plugining/ServicePlugin.cs
@@ -10,6 +10,6 @@ public abstract class ServicePlugin : PluginBase
     public sealed override PluginType PluginType => PluginType.Service;
     public virtual Task<bool> BeforeSend(PluginInfo pluginInfo, IResponse response) => Task.FromResult(true);
     public virtual Task<IResponse?> OnNoticeReceived(MessageContext messageContext) => Task.FromResult<IResponse?>(default);
-    public virtual Task<IResponse?> OnPluginException(MessageContext messageContext) => Task.FromResult<IResponse?>(default);
+    public virtual Task<IResponse?> OnPluginException(Exception exception, MessageContext messageContext) => Task.FromResult<IResponse?>(default);
     public virtual Task<IResponse?> OnBindingFailed(BindingException bindingException, MessageContext context) => Task.FromResult<IResponse?>(default);
 }
