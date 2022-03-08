d245fef7a9561c0a12b9b199e64c8f8cdfdfdb65
Milkitic <Milkitic@126.com>
2022-03-08 17:11:00 +0800

    Create ContactsManagerExtensions.cs

---
+++ src/MilkiBotFramework/ContactsManaging/ContactsManagerExtensions.cs
@@ -0,0 +1,21 @@
+﻿using MilkiBotFramework.Messaging;
+
+namespace MilkiBotFramework.ContactsManaging;
+
+public static class ContactsManagerExtensions
+{
+    public static async Task<string?> GetIdentityName(this IContactsManager contactsManager, MessageIdentity messageIdentity)
+    {
+        if (messageIdentity.MessageType == MessageType.Private)
+        {
+            return (await contactsManager.TryGetOrAddPrivateInfo(messageIdentity.Id!)).PrivateInfo?.Nickname;
+        }
+
+        if (messageIdentity.MessageType == MessageType.Channel)
+        {
+            return (await contactsManager.TryGetOrAddChannelInfo(messageIdentity.Id!, messageIdentity.SubId)).ChannelInfo?.Name;
+        }
+
+        return null;
+    }
+}
