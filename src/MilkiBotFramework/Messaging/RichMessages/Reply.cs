0fcb609e4898eed35cbe71f605057a7d84223a36
Milkitic <Milkitic@126.com>
2022-03-08 17:11:13 +0800

    tostring for some internal message types

--- src/MilkiBotFramework/Messaging/RichMessages/Reply.cs
+++ src/MilkiBotFramework/Messaging/RichMessages/Reply.cs
@@ -5,4 +5,8 @@ public class Reply : IRichMessage
     public Reply(string messageId) => MessageId = messageId;
     public string MessageId { get; set; }
     public virtual async ValueTask<string> EncodeAsync() => $"[Reply {MessageId}]";
+    public override string ToString()
+    {
+        return "[回复]";
+    }
 }
