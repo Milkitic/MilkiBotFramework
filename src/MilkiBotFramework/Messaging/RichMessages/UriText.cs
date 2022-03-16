a7ac46e3d23f33c9ef5e6ca7c590fff690efbffe
Milkitic <Milkitic@126.com>
2022-03-16 15:13:14 +0800

    Create UriText.cs

---
+++ src/MilkiBotFramework/Messaging/RichMessages/UriText.cs
@@ -0,0 +1,9 @@
+﻿namespace MilkiBotFramework.Messaging.RichMessages;
+
+public class UriText : IRichMessage
+{
+    public UriText(string content) => Content = content;
+    public string Content { get; set; }
+    public virtual async ValueTask<string> EncodeAsync() => Content;
+    public override string ToString() => "[链接]";
+}
