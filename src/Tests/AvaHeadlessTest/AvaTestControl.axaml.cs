39edfcd47153e08a43a71830238179aba7ec87b6
Milkitic <Milkitic@126.com>
2024-09-05 15:49:52 +0800

    Add `EnableWindowsTargeting`

--- src/Tests/AvaHeadlessTest/AvaTestControl.axaml.cs
+++ src/Tests/AvaHeadlessTest/AvaTestControl.axaml.cs
@@ -1,4 +1,7 @@
-using FluentAvalonia.UI.Controls;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Headless;
+using Avalonia.Input;
 using MilkiBotFramework.Data;
 using MilkiBotFramework.Imaging.Avalonia;
 
@@ -15,11 +18,19 @@ public partial class AvaTestControl : AvaRenderingControl<AvaTestViewModel>
     {
         InitializeComponent();
         NavigationView.SelectedItem = NavigationView.MenuItems[0];
+        Button.Focus(NavigationMethod.Tab);
         Loaded += AvaTestControl_Loaded;
     }
 
     private async void AvaTestControl_Loaded(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
     {
+        //await Task.Delay(200);
+        var topLevel = TopLevel.GetTopLevel(this);
+        topLevel.MouseDown(new Point(251, 165), MouseButton.Left);
+        //topLevel.MouseDown(new Point(251, 247), MouseButton.Left);
+        //topLevel.MouseUp(new Point(251, 247), MouseButton.Left);
+        //topLevel.MouseMove(new Point(271, 165));
+        await Task.Delay(200);
         await FinishRender();
     }
 }
